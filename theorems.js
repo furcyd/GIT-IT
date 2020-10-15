1d55af0cef40302b4c00b1ec59a3c7fdb722a28f
David Furcy <furcyd@uwosh.edu>
2020-10-14 23:23:36 -0500

    Add theorems 3 and 4

--- theorems.js
+++ theorems.js
@@ -12,8 +12,22 @@ var theorems =
 	  "page": 11,
 	  "text":
 	   	  "Suppose $T$ is a formal axiomatized theory whose language contains the language of basic arithmetic. Then, if $T$ is consistent and can prove a certain modest amount of arithmetic (and has a certain additional property that any sensible formalized arithmetic will share), there will be a sentence $G_T$ of basic arithmetic such that $T \\nvdash G_T$ and $T \\nvdash \lnot G_T$, so $T$ is negation incomplete."
-	}
+	},
 
+	{ "term": "",
+	  "n": 3,
+	  "page": 16,
+	  "text":
+	  "Suppose $T$ is an effectively axiomatized formal theory, and suppose we are given a G&‌ouml;cdel-numbering scheme. Then the corresponding properties/relations Wff, Sent, Prf are effectively decidable."
+	},
+
+	{
+	   "term": "",
+	  "n": 4,
+	  "page": 17,
+	  "text":
+	    "Suppose $T$ is an effectively axiomatized theory which includes the language of basic arithmetic, and suppose we have fixed on a G&‌ouml;del-numbering scheme. Then $T$ can express the corresponding numerical property Prf using some arithmetical wff <code>Prf(x, y)</code>."
+	}
     ];
 
 
